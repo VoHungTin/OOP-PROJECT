35250a43f17c9fc1b7dad1e16f7de2ff3a9cc925
nhoxtam2311 <33509262+nhoxtam2311@users.noreply.github.com>
2017-12-14 01:33:21 +0700

    Add files via upload

---
+++ src/pvz/view/image/Veggies.java
@@ -0,0 +1,26 @@
+package pvz.view.image;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import pvz.controller.PvZController;
+
+/**
+ * Visual object that represents a Pom-Pom Veggie
+ *
+ * <p> This class contains all the information necessary for the correct
+ * Visual operation of Pom-Pom Veggies. It contains values
+ * predetermined variables to distinguish Pom-Pom Veggies from
+ * other kinds of JVeggies. </ p>
+ */
+
+public class Veggies extends JVeggie{
+     /**
+     * Constructor CoachVeggie 
+     * @param posiX
+     * @param noZombie
+     * @param controller
+     */
+    public Veggies(float posiX, int noZombie, final PvZController controller){
+        super(posiX, "CV", noZombie, controller);
+    }
+}
