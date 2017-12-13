4ec2035cf5a9f2d3b5440a7b35780f4225c3af49
nhoxtam2311 <33509262+nhoxtam2311@users.noreply.github.com>
2017-12-14 01:41:44 +0700

    Update LimboVeggie.java

--- src/pvz/view/image/LimboVeggie.java
+++ src/pvz/view/image/LimboVeggie.java
@@ -1,20 +1,20 @@
-
 package pvz.view.image;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import pvz.controller.PvZController;
-/**
-  * Visual object that represents a GhettoBlaster Veggie
-  *
-  * <p> This class contains all the information necessary for the correct
-  * Visual operation of GhettoBlaster Veggies. It contains values
-  * predetermined variables to distinguish the Limbo Veggies from
-  * other kinds of JVeggies. </ p>
-  */
-public class LimboVeggie extends JVeggie{
 
-    public LimboVeggie(float posiX, int noZombie, final PvZController controller){
+/**
+ *   * Visual object that represents a GhettoBlaster Veggie   *  
+ * <p>
+ * This class contains all the information necessary for the correct   * Visual
+ * operation of GhettoBlaster Veggies. It contains values   * predetermined
+ * variables to distinguish the Limbo Veggies from   * other kinds of JVeggies.
+ * </ p>  
+ */
+public class LimboVeggie extends JVeggie {
+
+    public LimboVeggie(float posiX, int noZombie, final PvZController controller) {
         super(posiX, "LV", noZombie, controller);
     }
 }
