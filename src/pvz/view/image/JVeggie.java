4dc98efd9d9de2f2c19e3c4e39673c4fa28af246
nhoxtam2311 <33509262+nhoxtam2311@users.noreply.github.com>
2017-12-14 01:40:28 +0700

    Update JVeggie.java

--- src/pvz/view/image/JVeggie.java
+++ src/pvz/view/image/JVeggie.java
@@ -11,96 +11,112 @@ import pvz.model.Zombie;
 public abstract class JVeggie extends JComponent {
 
     /**
-      * Float that represents the exact position of the JVeggie in the lane (then rounded up when calling JVeggie.setLocation ()).
-      */
+     *       * Float that represents the exact position of the JVeggie in the
+     * lane (then rounded up when calling JVeggie.setLocation ()).      
+     */
     private float posiX;
     /**
-      * Float representing the movement speed of the JVeggie.
-      */
+     *       * Float representing the movement speed of the JVeggie.      
+     */
     private float speed;
     /**
-      * String containing the type of veggie that this JVeggie represents in order to associate him the good zombie of the model.
-      */
+     *       * String containing the type of veggie that this JVeggie represents
+     * in order to associate him the good zombie of the model.      
+     */
     private String typeZombie;
     /**
-      * Integer representing the zombie number of the JVeggie in order to associate him the good zombie of the model.
-      */
+     *       * Integer representing the zombie number of the JVeggie in order to
+     * associate him the good zombie of the model.      
+     */
     private int noZombie;
     /**
-      * Controller associated with JVeggie.
-      */
+     *       * Controller associated with JVeggie.      
+     */
     private PvZController controller;
     /**
-      * ArrayList containing sprites associated with a certain type of JVeggie walking.
-      */
+     *       * ArrayList containing sprites associated with a certain type of
+     * JVeggie walking.      
+     */
     private ArrayList<Image> walkList = new ArrayList<>();
-   /**
-      * ArrayList containing sprites associated with a certain type of JVeggie eating.
-      */
+    /**
+     *       * ArrayList containing sprites associated with a certain type of
+     * JVeggie eating.      
+     */
     private ArrayList<Image> eatList = new ArrayList<>();
     /**
-      * ArrayList containing the sprites associated with a certain type of JVeggie walking leaning.
-      */
+     *       * ArrayList containing the sprites associated with a certain type
+     * of JVeggie walking leaning.      
+     */
     private ArrayList<Image> walkLeaning = new ArrayList<>();
     /**
-      * Integer representing the index of the image that the JVeggie should display from a certain arrayList.
-      */
+     *       * Integer representing the index of the image that the JVeggie
+     * should display from a certain arrayList.      
+     */
     private int CurrentIndexImg;
     /**
-      * Image that the JVeggie displays at the next repaint.
-      */
+     *       * Image that the JVeggie displays at the next repaint.      
+     */
     private Image CurrentImg;
     /**
-      * String that lets you know which zombie model this JVeggie is associated with.
-      */
+     *       * String that lets you know which zombie model this JVeggie is
+     * associated with.      
+     */
     private String ID;
 
     /**
-      * Function returning the Zombie type associated with this JVeggie.
-      * @return the Veggie type of JVeggie.
-     
-     * @return  */
+     *       * Function returning the Zombie type associated with this JVeggie.
+     *       * @return the Veggie type of JVeggie.      
+     *
+     * @return
+     */
     public String getTypeZombie() {
         return typeZombie;
     }
+
     /**
-      * Method initializing the zombie ID.
-      * @param ID The ID saved in memory of this JVeggie.
-     
-     * @param ID */
+     *       * Method initializing the zombie ID.       * @param ID The ID saved
+     * in memory of this JVeggie.      
+     *
+     * @param ID
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
     /**
-      * Function returning the ID of this JVeggie.
-      * @return The ID of the JVeggie.
-     
-     * @return  */
+     *       * Function returning the ID of this JVeggie.       * @return The ID
+     * of the JVeggie.      
+     *
+     * @return
+     */
     public String getID() {
         return ID;
     }
 
     /**
-      * Method modifying the speed of this JVeggie.
-      * @param speed New speed of JVeggie.
-     
-     * @param speed */
+     *       * Method modifying the speed of this JVeggie.       * @param speed
+     * New speed of JVeggie.      
+     *
+     * @param speed
+     */
     public void setSpeed(float speed) {
         this.speed = speed;
     }
-    
+
     /**
-      * Constructor JVeggie.
-      * @parameter posiX Float which represents the movement speed of the JVeggie.
-      * @parameter typeZombie String containing the type of veggie that this JVeggie represents in order to associate him the good zombie of the model.
-      * @parameter noZombie Integer representing the zombie number of the JVeggie in order to associate him the good zombie of the model.
-      * @parameter Controller Controller.
-     
+     *       * Constructor JVeggie.       * @parameter posiX Float which
+     * represents the movement speed of the JVeggie.       * @parameter
+     * typeZombie String containing the type of veggie that this JVeggie
+     * represents in order to associate him the good zombie of the model.      
+     * * @parameter noZombie Integer representing the zombie number of the
+     * JVeggie in order to associate him the good zombie of the model.       *
+     * @parameter Controller Controller.      
+     *
      * @param posiX
      * @param typeZombie
      * @param noZombie
-     * @param controller */
+     * @param controller
+     */
     public JVeggie(float posiX, String typeZombie, int noZombie, final PvZController controller) {
         Image img;
         for (int i = 0; i < 4; i++) {
@@ -109,12 +125,12 @@ public abstract class JVeggie extends JComponent {
         }
 
         for (int i = 0; i < 4; i++) {
-            img = Toolkit.getDefaultToolkit().getImage(typeZombie + "Mange"+ (i + 1) + ".gif");
+            img = Toolkit.getDefaultToolkit().getImage(typeZombie + "Mange" + (i + 1) + ".gif");
             eatList.add(img);
         }
 
         for (int i = 0; i < 4; i++) {
-            img = Toolkit.getDefaultToolkit().getImage(typeZombie +"Penche"+ (i + 1) + ".gif");
+            img = Toolkit.getDefaultToolkit().getImage(typeZombie + "Penche" + (i + 1) + ".gif");
             walkLeaning.add(img);
         }
 
@@ -129,19 +145,19 @@ public abstract class JVeggie extends JComponent {
         controller.creeZombie(typeZombie, ID, noZombie);
     }
 
-
     /**
-      * Method modifying and moving the JVeggie according to its zombie of the associated model.
-      */
+     *       * Method modifying and moving the JVeggie according to its zombie
+     * of the associated model.      
+     */
     public void actualize() {
-        if (this instanceof LimboVeggie && controller.getTemps()%30==0) {
+        if (this instanceof LimboVeggie && controller.getTemps() % 30 == 0) {
             controller.playSound("untz.wav");
         }
         move();
         if (controller.getTemps() % 12 == 0) {
             if (CurrentIndexImg == 3) {
                 CurrentIndexImg = 0;
-            } else if (controller.getZombie(ID).getState()!=Zombie.EtatZombie.trap) {
+            } else if (controller.getZombie(ID).getState() != Zombie.EtatZombie.trap) {
                 CurrentIndexImg++;
             }
             switch (controller.getZombie(ID).getState()) {
@@ -168,8 +184,9 @@ public abstract class JVeggie extends JComponent {
     }
 
     /**
-      * Method that changes the position of the JVeggie in the Lane according to its speed.
-      */
+     *       * Method that changes the position of the JVeggie in the Lane
+     * according to its speed.      
+     */
     public void move() {
         posiX += speed;
         this.setLocation((int) posiX, this.getY());
